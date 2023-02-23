bc2f5846c2b639fd3d46aeb03a495bcb9ee0fd22
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-02-23 14:34:18 +0000

    solved the numbers problems

---
+++ strings.js
@@ -0,0 +1,32 @@
+function sayHello (string) {
+
+};
+
+function uppercase (string) {
+  // your code here
+};
+
+function lowercase (string) {
+  // your code here
+};
+
+function countCharacters (string) {
+  // your code here
+};
+
+function firstCharacter (string) {
+  // your code here
+};
+
+function firstCharacters (string, n) {
+  // your code here
+};
+
+module.exports = {
+  sayHello,
+  uppercase,
+  lowercase,
+  countCharacters,
+  firstCharacter,
+  firstCharacters
+};
